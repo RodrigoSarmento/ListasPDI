5c53f827ba802e825a5edbf5b10906776a1ae2c9
RodrigoSarmento <rodrigosarmentoxx@gmail.com>
2021-03-19 12:11:14 -0300

    start

---
+++ Build/applications/CMakeFiles/pixels.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pixels.dir/pixels.cpp.o"
+  "../bin/pixels.pdb"
+  "../bin/pixels"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pixels.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
